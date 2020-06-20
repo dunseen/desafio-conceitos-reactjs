e78a9405ae14376f59db9ff075b00ef3cb37a6c0
Davys lima <davysjunior08@hotmail.com>
2020-06-20 10:10:14 -0300

    Listar projetos da API - Finalizado.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React ,{useState, useEffect } from "react";
+import api from  './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(()=> {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    })
+
+  },[]);
+  
+  
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('projects', {
+      title: "Projeto",
+      ownner: "Dono"
+    });
+    const project = response.title;
+    setProjects([...projects,project]);
   }
 
   async function handleRemoveRepository(id) {
@@ -14,15 +30,14 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+          {projects.map(project => <li key={project.id}>
+            {project.title}
+            <button onClick={() => handleRemoveRepository(1)}>
+              Remover
+              </button>
+            </li>
+            )}     
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
